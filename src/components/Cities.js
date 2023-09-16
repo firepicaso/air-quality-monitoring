d62c9608b20811ae6120244cbab94c6fe6d85dc3
Mustakim <m.m.kiron@gmail.com>
2023-09-16 19:00:30 +0600

    Add States and Cities components

---
+++ src/components/Cities.js
@@ -0,0 +1,38 @@
+import { API_URL, API_KEY } from '../api';
+import React, { useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { setCities } from '../redux/cities/citiesSlice';
+
+const Cities = () => {
+  const dispatch = useDispatch();
+  const cities = useSelector((state) => state.cities);
+  const { state: selectedState } = useParams();
+
+  const fetchData = useCallback(async () => {
+    const res = await fetch(
+      `${API_URL}/cities?state=${selectedState}&country=Canada&key=${API_KEY}`,
+    );
+
+    const data = await res.json();
+    dispatch(setCities(data.data));
+  }, [dispatch, selectedState]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, selectedState]);
+
+  return (
+    <ul>
+      {cities.map(({city}) => (
+        <li key={city}>
+          <Link to={`/${selectedState}/${city}`}>
+            {city}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Cities;
---
+++ src/components/States.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { API_URL, API_KEY } from '../api';
+
+const States = () => {
+  const [states, setStates] = useState([]);
+
+  const fetchStates = async () => {
+    const res = await fetch(
+      `${API_URL}/states?country=Canada&key=${API_KEY}`,
+    );
+    const data = await res.json();
+    setStates(data.data);
+  };
+
+  useEffect(() => {
+    fetchStates();
+  }, []);
+
+  return (
+    <ul>
+      {states.map(({state}) => (
+        <li key={state}>
+          <Link to={`/${state}`}>
+            {state}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default States;
